da79694dc2a3619247c1304ae7c26fb9d2620535
Bohdan Mushkevych <mushkevych@gmail.com>
2015-10-27 20:35:36 -0700

    - adding .travis.yml

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(name = 'synergy_odm',
           'License :: OSI Approved :: BSD License',
           'Operating System :: OS Independent',
           'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.4',
           'Topic :: Software Development :: Libraries',
       ],
       requires=[]
